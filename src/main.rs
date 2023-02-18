8878f2b5d14209b815145a476397a94c4afb8155
nixbytes <real8686@gmail.com>
2023-02-18 16:08:51 -0500

    Add basic CLI component

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use ferris_says::say; // basic import
+use std::io::{self, Read}; // Need this to read input in
 use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello fellow Rustacean!");
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input) // get the input by reference   
+        .expect("failed to get input"); //expect is used for checking for errors
+    let message = String::from(input);
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
