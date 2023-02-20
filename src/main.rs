eb0f11891c7142c9ec66137dfdfd2400cdebdd0b
nixbytes <real8686@gmail.com>
2023-02-19 23:08:56 -0500

    work on args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use ferris_says::say; // basic import
+use ferris_says::say; // Basic import
+use std::env::args;
 use std::io::{self, Read}; // Need this to read input in
-use std::io::{stdout, BufWriter};
+use std::io::{stdout, BufWriter}; // Write to output
 
 fn main() {
+    // Define the vars for the in/output
+
     let stdout = stdout();
-    let mut input = String::new();
+    let mut input = std::env::args().nth(1).expect("no pattern give text");
+    //let args = Cli { pattern: pattern };
+
     io::stdin()
-        .read_to_string(&mut input) // get the input by reference   
+        .read_to_string(&mut input) // get the input by reference
         .expect("failed to get input"); //expect is used for checking for errors
     let message = String::from(input);
     let width = message.chars().count();
