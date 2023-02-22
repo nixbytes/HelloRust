95fe63ed1133eb1aa0bdda7e0f9ca16a270d0f5d
nixbytes <real8686@gmail.com>
2023-02-21 23:22:08 -0500

    Add arg aud decet output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use ferris_says::say; // Basic import
-use std::env::args;
+use std::env;
 use std::io::{self, Read}; // Need this to read input in
 use std::io::{stdout, BufWriter}; // Write to output
 
 fn main() {
-    // Define the vars for the in/output
-
+    // Define the args and collect in a string vec
+    let args: Vec<String> = env::args().collect();
     let stdout = stdout();
-    let mut input = std::env::args().nth(1).expect("no pattern give text");
-    //let args = Cli { pattern: pattern };
-
-    io::stdin()
-        .read_to_string(&mut input) // get the input by reference
-        .expect("failed to get input"); //expect is used for checking for errors
+    // list the first args for the stdout
+    let input = &args[1];
     let message = String::from(input);
     let width = message.chars().count();
 
